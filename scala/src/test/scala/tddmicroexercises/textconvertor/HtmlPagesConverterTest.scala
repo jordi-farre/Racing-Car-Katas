e10874201446a7c90dfa0b94dfcdbeb5372e44b3
Jordi Farré <jordifr@gmail.com>
2017-11-08 14:53:50 +0100

    created test for html text converter

--- scala/src/test/scala/tddmicroexercises/textconvertor/HtmlPagesConverterTest.scala
+++ scala/src/test/scala/tddmicroexercises/textconvertor/HtmlPagesConverterTest.scala
@@ -6,6 +6,7 @@ import org.scalatest.{Matchers, FlatSpec}
 class HtmlPagesConverterTest  extends FlatSpec with Matchers {
   it should "foo" in {
     val converter = new HtmlPagesConverter("foo")
+
     converter.filename shouldEqual "fixme"
   }
 }
--- scala/src/test/scala/tddmicroexercises/textconvertor/HtmlTextConverterTest.scala
+++ scala/src/test/scala/tddmicroexercises/textconvertor/HtmlTextConverterTest.scala
@@ -1,11 +1,17 @@
 package tddmicroexercises.textconvertor
 
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.io.Source
 
 
 class HtmlTextConverterTest  extends FlatSpec with Matchers {
-  it should "foo" in {
-    val converter = new HtmlTextConverter("foo");
-    converter.fullFilenameWithPath shouldEqual "fixme"
+  it should "scape html in each line of a file" in {
+    val inputPath = this.getClass.getResource("input_with_html.txt").getPath;
+    val expectedResult = Source.fromURL(this.getClass.getResource("output_with_escaped_html.txt")).getLines().mkString("\n")
+
+    val converter = new HtmlTextConverter(inputPath);
+
+    converter.convertToHtml() shouldEqual expectedResult
   }
 }
