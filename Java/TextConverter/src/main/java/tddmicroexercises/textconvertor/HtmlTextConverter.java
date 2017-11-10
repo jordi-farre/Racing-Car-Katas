ae47399d26118ccc5b2cf093e68bc49420e1d2ae
Jordi Farré <jordifr@gmail.com>
2017-11-10 14:28:11 +0100

    close file with try-with-resources

--- Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
+++ Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
@@ -15,18 +15,19 @@ public class HtmlTextConverter
 
     public String convertToHtml() throws IOException{
     
-	    BufferedReader reader = new BufferedReader(new FileReader(fullFilenameWithPath));
-	    
-	    String line = reader.readLine();
-	    String html = "";
-	    while (line != null)
-	    {
-	    	html += StringEscapeUtils.escapeHtml(line);
-	        html += "<br />";
-	        line = reader.readLine();
-	    }
-	    return html;
+	    try (FileReader fileReader = new FileReader(fullFilenameWithPath);
+			 BufferedReader reader = new BufferedReader(fileReader)) {
 
+			String line = reader.readLine();
+			String html = "";
+			while (line != null) {
+				html += StringEscapeUtils.escapeHtml(line);
+				html += "<br />";
+				line = reader.readLine();
+			}
+
+			return html;
+		}
     }
 
 	public String getFilename() {
