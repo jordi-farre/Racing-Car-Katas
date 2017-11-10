9a23aa2a125ef66a6e027fd96cd8940a71df255d
Jordi Farré <jordifr@gmail.com>
2017-11-10 11:23:54 +0100

    improve formatting and remane constants

--- Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
+++ Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
@@ -1,26 +1,23 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
-public class Alarm
-{
-    private final double LowPressureThreshold = 17;
-    private final double HighPressureThreshold = 21;
+public class Alarm {
+
+    private final double LOW_PRESSURE_THRESHOLD = 17;
+    private final double HIGH_PRESSURE_THRESHOLD = 21;
 
     Sensor sensor = new Sensor();
 
     boolean alarmOn = false;
 
-    public void check()
-    {
+    public void check() {
         double psiPressureValue = sensor.popNextPressurePsiValue();
 
-        if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue)
-        {
+        if (psiPressureValue < LOW_PRESSURE_THRESHOLD || HIGH_PRESSURE_THRESHOLD < psiPressureValue) {
             alarmOn = true;
         }
     }
 
-    public boolean isAlarmOn()
-    {
-        return alarmOn; 
+    public boolean isAlarmOn() {
+        return alarmOn;
     }
 }
