2fc47d43eeb841a4391350f6368f40cb98a1364c
Jordi Farré <jordifr@gmail.com>
2017-11-10 11:21:32 +0100

    tire pressure test implementation for java

--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
@@ -1,14 +1,36 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
 
-import org.junit.*;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class TestAlarm {
 
-    @Test
-    public void foo() {
-        Alarm alarm = new Alarm();
-        assertEquals(false, alarm.isAlarmOn());
+    private Sensor mockedSensor;
+    private Alarm alarm;
+
+    @BeforeEach
+    public void initialize() {
+        mockedSensor = mock(Sensor.class);
+        alarm = new Alarm() {{ this.sensor = mockedSensor; }};
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "16.9, true",
+            "21.1, true",
+            "17, false",
+            "21, false",
+            "18, false",
+            "19.2, false" })
+    public void alarm_raises_when_pressure_outside_threshold(double pressure, boolean alarmOn) {
+        when(mockedSensor.popNextPressurePsiValue()).thenReturn(pressure);
+
+        alarm.check();
+
+        assertEquals(alarmOn, alarm.isAlarmOn());
     }
 }
